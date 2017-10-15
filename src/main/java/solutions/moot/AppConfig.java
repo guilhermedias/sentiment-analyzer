d64f741ba2f3bba9b6ea2b3436e8433cad56fcae
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2017-10-15 15:44:03 -0200

    Use Spring dependency injection

---
+++ src/main/java/solutions/moot/AppConfig.java
@@ -0,0 +1,54 @@
+package solutions.moot;
+
+import org.slf4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import solutions.moot.feelings.domain.aggregator.MeanValueVadAggregator;
+import solutions.moot.feelings.domain.aggregator.VadAggregator;
+import solutions.moot.feelings.domain.analyzer.SimpleVadVadSentenceAnalyzer;
+import solutions.moot.feelings.domain.analyzer.VadSentenceAnalyzer;
+import solutions.moot.feelings.domain.dictionary.VadDictionary;
+import solutions.moot.feelings.domain.lemmatizer.Lemmatizer;
+import solutions.moot.feelings.infra.dictionary.InMemoryCsvVadDictionary;
+import solutions.moot.feelings.infra.lemmatizer.NlpCoreBasedLemmatizer;
+import solutions.moot.feelings.infra.proxy.LoggingVadDictionaryProxy;
+
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Lemmatizer nlpCoreBasedLemmatizer() {
+        return new NlpCoreBasedLemmatizer();
+    }
+
+    @Bean
+    public Logger dictionaryLookupLogger() {
+        return getLogger("VAD dictionary look up logger");
+    }
+
+    @Bean
+    public VadDictionary inMemoryCsvVadDictionary() throws IOException {
+        return new InMemoryCsvVadDictionary("vad-dictionary.csv");
+    }
+
+    @Bean
+    public VadDictionary loggingVadDictionary(VadDictionary inMemoryCsvVadDictionary, Logger logger)
+            throws IOException {
+        return new LoggingVadDictionaryProxy(inMemoryCsvVadDictionary, logger);
+    }
+
+    @Bean
+    public VadAggregator meanValueVadAggregator() {
+        return new MeanValueVadAggregator();
+    }
+
+    @Bean
+    public VadSentenceAnalyzer simpleVadSentenceAnalyzer(Lemmatizer lemmatizer,
+                                                         VadDictionary loggingVadDictionary,
+                                                         VadAggregator aggregator) {
+        return new SimpleVadVadSentenceAnalyzer(lemmatizer, loggingVadDictionary, aggregator);
+    }
+}
--- src/main/java/solutions/moot/Application.java
+++ src/main/java/solutions/moot/Application.java
@@ -1,46 +1,21 @@
 package solutions.moot;
 
-import org.slf4j.Logger;
-import solutions.moot.feelings.domain.aggregator.MeanValueVadAggregator;
-import solutions.moot.feelings.domain.aggregator.VadAggregator;
-import solutions.moot.feelings.domain.analyzer.SimpleVadVadSentenceAnalyzer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import solutions.moot.feelings.domain.analyzer.VadSentenceAnalyzer;
-import solutions.moot.feelings.domain.dictionary.VadDictionary;
 import solutions.moot.feelings.domain.dictionary.VadValue;
-import solutions.moot.feelings.domain.lemmatizer.Lemmatizer;
-import solutions.moot.feelings.infra.dictionary.InMemoryCsvVadDictionary;
-import solutions.moot.feelings.infra.lemmatizer.NlpCoreBasedLemmatizer;
-import solutions.moot.feelings.infra.proxy.LoggingVadDictionaryProxy;
-
-import java.io.IOException;
 
 import static java.lang.String.format;
-import static org.slf4j.LoggerFactory.getLogger;
 
 public class Application {
-    public static void main(String[] args) throws Exception {
-        Lemmatizer lemmatizer = new NlpCoreBasedLemmatizer();
-        VadDictionary dictionary = constructVadDictionary();
-        VadAggregator aggregator = new MeanValueVadAggregator();
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        VadSentenceAnalyzer analyzer = new SimpleVadVadSentenceAnalyzer(
-                lemmatizer,
-                dictionary,
-                aggregator
-        );
+        VadSentenceAnalyzer analyzer = applicationContext.getBean(VadSentenceAnalyzer.class);
 
         String sentence = "We are super excited to announce that our last release was a success";
         VadValue sentenceVadValue = analyzer.analyzeSentence(sentence);
         printSentenceScore(sentence, sentenceVadValue);
-    }
-
-    private static VadDictionary constructVadDictionary() throws IOException {
-        InMemoryCsvVadDictionary actualDictionary =
-                new InMemoryCsvVadDictionary("vad-dictionary.csv");
-
-        Logger logger = getLogger("VAD dictionary look up logger");
-
-        return new LoggingVadDictionaryProxy(actualDictionary, logger);
     }
 
     private static void printSentenceScore(String sentence, VadValue sentenceVadValue) {
