3db309913d02df99b38bd8a568b7c1d3398342d2
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2017-10-15 16:17:51 -0200

    Rename application launcher class

--- src/main/java/solutions/moot/AppIndex.java
+++ src/main/java/solutions/moot/AppIndex.java
@@ -3,7 +3,7 @@ package solutions.moot;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Application {
+public class AppIndex {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
